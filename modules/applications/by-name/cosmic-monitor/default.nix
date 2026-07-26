99e47ad9b279703f6aed4b3b6b8512c05f2c07e9
candsastle <tsaharya@pm.me>
2026-07-26 03:08:24 -0400

    added initial cosmic-monitor default.nix

---
+++ modules/applications/by-name/cosmic-monitor/default.nix
@@ -0,0 +1,30 @@
+{lib, ...}:
+lib.cosmic.applications.mkCosmicApplication {
+  name = "cosmic-monitor";
+  originalName = "COSMIC System Monitor";
+  identifier = "com.system76.CosmicMonitor";
+  configurationVersion = 1;
+
+  maintainers = [lib.maintainers.HeitorAugustoLN];
+
+  settingsOptions = let
+    inherit (lib.cosmic) defaultNullOpts;
+  in {
+    app_theme =
+      defaultNullOpts.mkRonEnum ["Dark" "Light" "System"]
+      {
+        __type = "enum";
+        variant = "System";
+      }
+      ''
+        The theme of the application.
+      '';
+  };
+
+  settingsExample = {
+    app_theme = {
+      __type = "enum";
+      variant = "System";
+    };
+  };
+}
